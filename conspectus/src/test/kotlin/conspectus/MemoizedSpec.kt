6772dd8d7808d03164eec7ca968505df962c5ed3
Artur Dryomov <artur.dryomov@gmail.com>
2019-11-24 12:04:55 +0300

    Add memoized access lock outside of the execution phase.

--- conspectus/src/test/kotlin/conspectus/MemoizedSpec.kt
+++ conspectus/src/test/kotlin/conspectus/MemoizedSpec.kt
@@ -1,50 +1,46 @@
 package conspectus
 
 import org.assertj.core.api.Assertions.assertThat
+import org.assertj.core.api.Assertions.assertThatIllegalStateException
 import org.mockito.Mockito.*
 
 class MemoizedSpec : Spec({
 
     val env by memoized { Environment() }
 
+    it("throws on accessing") {
+        assertThatIllegalStateException().isThrownBy { env.value }
+    }
+
     it("does not call creator") {
         verifyNoInteractions(env.creator)
     }
 
-    context("access") {
+    context("set up") {
 
         beforeEach {
-            env.value
+            env.valueContainer.setUp()
         }
 
-        it("calls creator") {
-            verifyOnly(env.creator).invoke()
+        it("does not call creator") {
+            verifyNoInteractions(env.creator)
         }
 
-        it("returns creator result") {
-            assertThat(env.value).isEqualTo(Environment.CREATOR_RESULT)
-        }
-
-        context("access again") {
+        context("access") {
 
             beforeEach {
-                clearInvocations(env.creator)
-
                 env.value
             }
 
-            it("does not call creator") {
-                verifyNoInteractions(env.creator)
-            }
-        }
-
-        context("reset") {
-
-            beforeEach {
-                env.valueContainer.reset()
+            it("calls creator") {
+                verifyOnly(env.creator).invoke()
             }
 
-            context("access") {
+            it("returns creator result") {
+                assertThat(env.value).isEqualTo(Environment.CREATOR_RESULT)
+            }
+
+            context("access again") {
 
                 beforeEach {
                     clearInvocations(env.creator)
@@ -52,8 +48,55 @@ class MemoizedSpec : Spec({
                     env.value
                 }
 
-                it("calls creator") {
-                    verifyOnly(env.creator).invoke()
+                it("does not call creator") {
+                    verifyNoInteractions(env.creator)
+                }
+
+                it("returns creator result") {
+                    assertThat(env.value).isEqualTo(Environment.CREATOR_RESULT)
+                }
+            }
+
+            context("tear down") {
+
+                beforeEach {
+                    clearInvocations(env.creator)
+
+                    env.valueContainer.tearDown()
+                }
+
+                it("throws on accessing") {
+                    assertThatIllegalStateException().isThrownBy { env.value }
+                }
+
+                it("does not call creator") {
+                    verifyNoInteractions(env.creator)
+                }
+
+                context("set up") {
+
+                    beforeEach {
+                        env.valueContainer.setUp()
+                    }
+
+                    it("does not call creator") {
+                        verifyNoInteractions(env.creator)
+                    }
+
+                    context("access") {
+
+                        beforeEach {
+                            env.value
+                        }
+
+                        it("calls creator") {
+                            verifyOnly(env.creator).invoke()
+                        }
+
+                        it("returns creator result") {
+                            assertThat(env.value).isEqualTo(Environment.CREATOR_RESULT)
+                        }
+                    }
                 }
             }
         }
@@ -62,6 +105,7 @@ class MemoizedSpec : Spec({
 }) {
 
     class Environment {
+
         companion object {
             const val CREATOR_RESULT = "value"
         }
